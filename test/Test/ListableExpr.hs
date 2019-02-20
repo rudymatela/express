045f30d1b9ded4b0aee0178f2bb7221e54e6d0cd
Rudy Matela <rudy@matela.com.br>
2019-02-19 22:10:53 -0300

    Add back IntE0 and IntEV types

--- test/Test/ListableExpr.hs
+++ test/Test/ListableExpr.hs
@@ -6,6 +6,9 @@ module Test.ListableExpr
   -- * Expressions of a type
   , IntE (..)
 
+  , IntE0 (..)
+  , IntEV (..)
+
   -- ** Functional values
   , IntToIntE (..)
   , IntToIntToIntE (..)
@@ -17,20 +20,36 @@ import Test.LeanCheck.Function.ShowFunction
 import Data.Haexpress.Fixtures
 import Data.Function (on)
 
+
 newtype IntE  =  IntE { unIntE :: Expr }
+
+newtype IntE0  =  IntE0 { unIntE0 :: Expr }
+newtype IntEV  =  IntEV { unIntEV :: Expr }
+
 newtype IntToIntE  =  IntToIntE { unIntToIntE :: Expr }
 newtype IntToIntToIntE  =  IntToIntToIntE { unIntToIntToIntE :: Expr }
 
+
 instance Show IntE  where  show (IntE e) = show e
+
+instance Show IntE0 where  show (IntE0 e) = show e
+instance Show IntEV where  show (IntEV e) = show e
+
 instance Show IntToIntE  where  show (IntToIntE e) = show e
 instance Show IntToIntToIntE  where  show (IntToIntToIntE e) = show e
 
 instance Listable IntE  where
   tiers  =  mapT IntE
          $  cons0 i_
-         \/ toTiers [var v (undefined :: Int) | v <- ["x", "y", "z", "x'"]] `addWeight` 1
-         \/ mapT val (tiers :: [[Int]]) `addWeight` 1
+         \/ cons1 unIntEV
+         \/ cons1 unIntE0
          \/ cons2 (\(IntToIntE f) (IntE xx) -> f :$ xx)
+
+instance Listable IntE0 where
+  tiers  =  (IntE0 . val) `mapT` (tiers :: [[Int]])
+
+instance Listable IntEV where
+  list  =  map (IntEV . (`var` (undefined :: Int))) ["x", "y", "z", "x'"] -- TODO: infinite list
 
 instance Listable IntToIntE where
   tiers  =  mapT IntToIntE
@@ -42,10 +61,11 @@ instance Listable IntToIntE where
 instance Listable IntToIntToIntE where
   list  =  map IntToIntToIntE [plusE, timesE]
 
+
 instance Listable Expr where
   tiers  =  reset (cons1 unIntE)
          \/ cons1 unIntToIntE      `addWeight` 1
          \/ cons1 unIntToIntToIntE `addWeight` 1
 
-instance ShowFunction Expr where
-  bindtiers  =  bindtiersShow
+
+instance ShowFunction Expr where bindtiers  =  bindtiersShow
