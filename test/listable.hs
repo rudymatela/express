ba124bd27490f8cbd69ca89bd71f258912f70f30
Rudy Matela <rudy@matela.com.br>
2019-03-06 12:13:19 -0300

    test/listable: point-free style

--- test/listable.hs
+++ test/listable.hs
@@ -9,18 +9,20 @@ tests :: Int -> [Bool]
 tests n =
   [ True
 
-  , holds n $ \e -> isJust (toDynamic e)
+  , holds n $ isJust . toDynamic
 
-  , holds n $ \(IntE xx) -> isJust $ evaluateInt xx
-  , holds n $ \(BoolE pp) -> isJust $ evaluateBool pp
-  , holds n $ \(IntsE xxs) -> isJust $ evaluateInts xxs
-  , holds n $ \(IntToIntE ff) -> isJust $ evaluateIntToInt ff
+  , holds n $ isJust . evaluateInt      . unIntE
+  , holds n $ isJust . evaluateBool     . unBoolE
+  , holds n $ isJust . evaluateInts     . unIntsE
+  , holds n $ isJust . evaluateIntToInt . unIntToIntE
 
   , holds n $ \(IntToIntE ff) (IntE xx) -> isJust . evaluateInt $ ff :$ xx
   , holds n $ \(IntToIntToIntE ff) (IntE xx) (IntE yy) -> isJust . evaluateInt $ ff :$ xx :$ yy
-  , holds n $ \(IntE xx) -> isNothing $ evaluateIntToInt xx
-  , holds n $ \(IntE xx) -> isNothing $ evaluateBool xx
-  , holds n $ \(BoolE pp) -> isNothing $ evaluateInt pp
+
+  , holds n $ isNothing . evaluateInt      . unBoolE
+  , holds n $ isNothing . evaluateBool     . unIntE
+  , holds n $ isNothing . evaluateInts     . unIntE
+  , holds n $ isNothing . evaluateIntToInt . unIntE
 
   , (counterExample n $ \(IntE xx) -> False) == Just ["_ :: Int"]
   ]
