927fcc346ec36f6d6d07de54901e4f2493222997
Rudy Matela <rudy@matela.com.br>
2019-03-06 12:11:05 -0300

    more tests

--- test/listable.hs
+++ test/listable.hs
@@ -10,16 +10,29 @@ tests n =
   [ True
 
   , holds n $ \e -> isJust (toDynamic e)
+
   , holds n $ \(IntE xx) -> isJust $ evaluateInt xx
+  , holds n $ \(BoolE pp) -> isJust $ evaluateBool pp
+  , holds n $ \(IntsE xxs) -> isJust $ evaluateInts xxs
+  , holds n $ \(IntToIntE ff) -> isJust $ evaluateIntToInt ff
+
   , holds n $ \(IntToIntE ff) (IntE xx) -> isJust . evaluateInt $ ff :$ xx
   , holds n $ \(IntToIntToIntE ff) (IntE xx) (IntE yy) -> isJust . evaluateInt $ ff :$ xx :$ yy
   , holds n $ \(IntE xx) -> isNothing $ evaluateIntToInt xx
+  , holds n $ \(IntE xx) -> isNothing $ evaluateBool xx
+  , holds n $ \(BoolE pp) -> isNothing $ evaluateInt pp
 
   , (counterExample n $ \(IntE xx) -> False) == Just ["_ :: Int"]
   ]
 
 evaluateInt :: Expr -> Maybe Int
 evaluateInt = evaluate
+
+evaluateBool :: Expr -> Maybe Bool
+evaluateBool = evaluate
+
+evaluateInts :: Expr -> Maybe [Int]
+evaluateInts = evaluate
 
 evaluateIntToInt :: Expr -> Maybe (Int -> Int)
 evaluateIntToInt = evaluate
