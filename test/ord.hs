7324cae118e3f10db19e013396e704af552f6b8e
Rudy Matela <rudy@matela.com.br>
2019-07-06 17:20:09 -0300

    test/ord: migrate a few tests from Speculate

--- test/ord.hs
+++ test/ord.hs
@@ -10,4 +10,30 @@ tests n =
 
   , holds n $ (okEqOrd :: Expr -> Expr -> Expr -> Bool)
   , holds n $ \(Ill e0) (Ill e1) (Ill e2) -> okEqOrd e0 e1 e2
+
+  -- Holes < Values < Apps
+  , xx < zero
+  , zero < zero -+- one
+  , xx < xx -+- yy
+  , zero < xx -+- yy
+
+  -- Less arity is less
+  , zero < absE
+  , absE < times
+  , ae   < ordE
+  , ordE < times
+  , value "id" (id -:>  int)  < value "id"    (id    -:>  [int])
+  , value "id" (id -:> [int]) < value "id"    (id    -:> [[int]])
+  , value "id" (id -:>  int)  < value "sum"   (sum   -:>  [int])
+  , value "id" (id -:>  int)  < value "(:[])" ((:[]) -:>   int)
+
+  -- precedent types
+  , pp < xx
+  , cc < xx
+  , pp < cc
+  , xx < xxs
+  , ae < zero
+  , true < zero
+  , true < ae
+  , zero < nil
   ]
