ee1e4fcb62cf29ed7b9fb2904b8046055bbd79e9
Rudy Matela <rudy@matela.com.br>
2019-07-22 19:34:43 -0300

    Explicitly export stuff from Data.Haexpress

--- src/Data/Haexpress.hs
+++ src/Data/Haexpress.hs
@@ -8,10 +8,117 @@
 {-# LANGUAGE CPP #-}
 module Data.Haexpress
   ( 
+  -- -- -- Data.Haexpress.Core exports -- -- --
+
+  -- * The Expr datatype
+    Expr (..)
+
+  -- * Smart constructors
+  , value
+  , val
+  , ($$)
+  , var
+
+  -- * Evaluating Exprs
+  , evaluate
+  , eval
+  , evl
+  , typ
+  , etyp
+  , mtyp
+  , toDynamic
+
+  -- * Boolean properties
+  , isValue
+  , isApp
+  , isVar
+  , isConst
+  , isIllTyped
+  , isWellTyped
+  , hasVar
+  , isGround
+
+  -- * Comparison
+  , compareComplexity
+
+  -- * Properties
+  , arity
+  , size
+  , depth
+  , height
+
+  -- * Listing subexpressions
+  , subexprs
+  , values
+  , vars
+  , consts
+  , nubSubexprs
+  , nubValues
+  , nubVars
+  , nubConsts
+
+  -- * Showing Exprs
+  , showExpr
+  , showPrecExpr
+  , showOpExpr
+
+  -- -- -- Data.Haexpress.Map exports -- -- --
+
+  -- * Mapping subexpressions
+  , mapValues
+  , mapVars
+  , mapConsts
+  , mapSubexprs
+  , (//-)
+  , (//)
+  , renameVarsBy
+
+
+  -- -- -- Data.Haexpress.Fold exports -- -- --
+
+  -- * Folding Exprs
+  , fold
+  , unfold
+  , foldPair
+  , unfoldPair
+  , foldApp
+  , unfoldApp
+
+
+  -- -- -- Data.Haexpress.Hole exports -- -- --
+
+  -- * Creating variables
+  , varAsTypeOf
+  , listVars
+  , listVarsAsTypeOf
+
+  -- * Typed holes
+  , hole
+  , isHole
+  , holes
+  , nubHoles
+  , holeAsTypeOf
+
+  -- -- -- Data.Haexpress.Canon expors -- -- --
+
+  -- * Canonicalizing Exprs
+  , canonicalize
+  , canonicalizeWith
+  , canonicalization
+  , canonicalizationWith
+  , isCanonical
+  , isCanonicalWith
+  , canonicalVariations
+  , fastCanonicalVariations
+
+  -- * Matching Exprs
+  , match
+  , matchWith
+  , isInstanceOf
+  , hasInstanceOf
+  , isSubexprOf
+
 -- TODO: explicitly export everything instead of the modules
-    module Data.Haexpress.Basic
-  , module Data.Haexpress.Canon
-  , module Data.Haexpress.Match
   , module Data.Haexpress.Instances
   , module Data.Haexpress.Name
   , module Data.Haexpress.Name.Derive
